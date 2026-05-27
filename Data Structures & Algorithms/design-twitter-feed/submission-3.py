46107e52e37f66b876fcfe0cfab4852d2b6201b4
salasya2 <salasya2@asu.edu>
2026-05-27 15:21:28 -0700

    Add: design-twitter-feed - submission-3

---
+++ Data Structures & Algorithms/design-twitter-feed/submission-3.py
@@ -0,0 +1,43 @@
+class Twitter:
+
+    def __init__(self):
+        self.timestamp = 0
+        self.tweets = defaultdict(list)
+        self.followees = defaultdict(set)
+
+    def postTweet(self, userId: int, tweetId: int) -> None:
+        self.tweets[userId].append([self.timestamp,tweetId])
+        if len(self.tweets[userId]) > 10:
+            self.tweets[userId].pop(0)
+        self.timestamp += 1   
+
+    def getNewsFeed(self, userId: int) -> List[int]:
+        
+        userIds = [userId]
+        if userId in self.followees:
+            userIds.extend(self.followees[userId])
+        
+
+        tweets = []
+
+        for  u in userIds:
+            if u not in self.tweets:
+                continue
+            for t in self.tweets[u]:
+                heapq.heappush(tweets,t)
+                if len(tweets) > 10:
+                    heapq.heappop(tweets)
+                   
+        res = []
+        while tweets:
+            res.append(heapq.heappop(tweets)[1])
+        return res[::-1]
+
+    def follow(self, followerId: int, followeeId: int) -> None:
+        if followerId != followeeId:
+            self.followees[followerId].add(followeeId)
+
+    def unfollow(self, followerId: int, followeeId: int) -> None:
+        self.followees[followerId].discard(followeeId)
+
+        
