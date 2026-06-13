1fff32fed56aba66d569cb502a2ece2ecc4deb00
salasya2 <salasya2@asu.edu>
2026-06-13 10:09:37 -0700

    Add: max-area-of-island - submission-7

---
+++ Data Structures & Algorithms/max-area-of-island/submission-7.py
@@ -0,0 +1,61 @@
+class DSU:
+    def __init__(self,n):
+        self.parent = list(range(n+1))
+        self.size = [1]*(n+1)
+    
+    def find(self, node):
+        if self.parent[node]!=node:
+            self.parent[node] = self.find(self.parent[node])
+        
+        return self.parent[node]
+    
+    def union(self, u,v):
+        pu = self.find(u)
+        pv = self.find(v)
+
+        if pu == pv:
+            return False
+        
+        if self.size[pu] >= self.size[pv]:
+            self.size[pu] += self.size[pv]
+            self.parent[pv] = pu
+        else:
+            self.size[pv] += self.size[pu]
+            self.parent[pu] = pv
+        return True
+
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+
+            res = 0
+            n = len(grid)
+            m = len(grid[0])
+            directions = [[0,1],[0,-1],[1,0],[-1,0]]
+            dsu = DSU(n*m)
+
+            def index(r,c):
+             
+               return r*m + c
+
+            for i in range(n):
+                for j in range(m):
+
+                    if grid[i][j] == 1:
+
+                        res = max(res, dsu.size[index(i,j)])
+                        for dr,dc in directions:
+
+                            nr = i + dr
+                            nc = j + dc
+
+                            if nr >= n or nc >= m or nr < 0 or nc < 0 or grid[nr][nc] == 0:
+                                continue
+                            dsu.union(index(nr,nc),index(i,j))
+                        res = max(dsu.size[dsu.find(index(i,j))],res)
+                               
+            
+            return res
+
+
+
+
