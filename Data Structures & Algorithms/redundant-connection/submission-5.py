95acae15289c85ab6af01c0d9fa24e19e5d9880b
salasya2 <salasya2@asu.edu>
2026-06-23 09:30:16 -0700

    Add: redundant-connection - submission-5

---
+++ Data Structures & Algorithms/redundant-connection/submission-5.py
@@ -0,0 +1,41 @@
+class DSU:
+    def __init__(self, n):
+        self.parent = list(range(n + 1))
+        self.size = [1] * (n + 1)
+
+    def find(self, node):
+
+        if self.parent[node] != node:
+            self.parent[node] = self.find(self.parent[node])
+        return self.parent[node]
+
+    def union(self, u, v):
+        pu = self.find(u)
+        pv = self.find(v)
+
+        if pu == pv:
+            return False
+
+        if self.size[pu] >= self.size[pv]:
+            self.size[pu] += self.size[pv]
+            self.parent[pv] = pu
+
+        else:
+            self.size[pv] += self.size[pu]
+            self.parent[pu] = pv
+
+        return True
+
+
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+
+        n = len(edges)
+        dsu = DSU(n)
+
+        for u, v in edges:
+            
+
+            if dsu.union(u, v) == False:
+                return [u, v]
+        return []
