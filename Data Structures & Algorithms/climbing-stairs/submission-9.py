a9eb4a831151c1fd7a7353baa70b06d0bae21052
salasya2 <salasya2@asu.edu>
2026-06-25 08:48:15 -0700

    Add: climbing-stairs - submission-9

---
+++ Data Structures & Algorithms/climbing-stairs/submission-9.py
@@ -0,0 +1,15 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        
+        if n == 1:
+            return 1
+        
+        l = [0] * n
+
+        l[0] = 1
+        l[1] = 2
+
+        for i in range(2,n):
+            l[i] = l[i-1] + l[i-2]
+        
+        return l[n-1]
