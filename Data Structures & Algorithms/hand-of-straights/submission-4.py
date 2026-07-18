b1c8279ec3a847a50a679519bd5175ff623fa12f
salasya2 <salasya2@asu.edu>
2026-07-17 20:49:30 -0700

    Add: hand-of-straights - submission-4

---
+++ Data Structures & Algorithms/hand-of-straights/submission-4.py
@@ -0,0 +1,32 @@
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+
+        '''
+        hashmap
+        '''
+        if len(hand)%groupSize:
+            return False
+        hand.sort()
+        freq = defaultdict(int)
+        for num in hand:
+            freq[num] += 1
+        
+
+        for num in hand:
+            start = num
+            while freq[start-1]:
+                start -= 1
+            while  start <= num:
+                while freq[start]:
+
+                    for i in range(start,start+groupSize):
+                        if not freq[i]:
+                            return False
+                        freq[i] -= 1   
+                start += 1
+                
+            
+        return True
+        
+
+        
